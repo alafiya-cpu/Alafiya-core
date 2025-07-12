85156308eb859678ec81398d7e4a996cc5f1edfb
BitBirr <bitbirrpay@gmail.com>
2025-07-12 23:43:03 +0300

    Updates to useLocalstoage hook to fix import issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { supabase } from '../utils/supabase'
+// import { supabase } from '../utils/supabase'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
